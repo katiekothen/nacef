2538f5c12f250bd33d24589bd38149cde777a6a5
Mostafa Sakr <msakr3@gmail.com>
2025-02-21 21:19:45 -0700

    Finish updating home page code and localisation structure.

--- app/frontend/components/HomePage/HomePage.jsx
+++ app/frontend/components/HomePage/HomePage.jsx
@@ -4,16 +4,16 @@ import UserHeaderUI from '~/components/RegistrationSession/UserHeaderUI.jsx';
 
 function HomePage(props) {
   const locale = document.getElementById("locale").getAttribute("content");
-  console.log(locale)
-  const firstParagraph = document.getElementById("first_paragraph").getAttribute("content");
+  const here = document.getElementById("here").getAttribute("content");
+  const firstMessage = document.getElementById("first_message").getAttribute("content");
+  const secondMessage = document.getElementById("first_message").getAttribute("content");
+  const link = <a href={`/${locale}/registration_sessions`}>{here}</a>
   return (
     <div>
       {UserHeaderUI(props.admin)}
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <div style={{ position: "absolute", top: "35%" }}>
-        <p style={{ textAlign: "center" }}>{firstParagraph}</p>
-        <p style={{ textAlign: "center" }}>Each prospective student may register for only one registration session.</p>
-        <p style={{ textAlign: "center" }}>Click <a href={`/${locale}/registration_sessions`}>here</a> to see a list of registration sessions to choose from. </p>
+        <p style={{ textAlign: "center" }}>{firstMessage} {link} {secondMessage}</p>
         </div>
       </div>
     </div>
