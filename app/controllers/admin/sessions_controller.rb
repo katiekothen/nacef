3bbbfb8a2627f424475c55f80e74727068c3f2bb
Mostafa Sakr <msakr3@gmail.com>
2025-02-24 17:43:40 -0700

    Update sessions_controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,7 +5,7 @@ class Admin::SessionsController < ApplicationController
   def create
     username = params[:username]
     password = params[:password]
-    if username == Rails.application.credentials.user && BCrypt::Password.new(Rails.application.credentials.password) == password
+    if username == Rails.application.credentials.user && Rails.application.credentials.password == password
       session[:admin] = 1
       redirect_to admin_registration_sessions_path
     else
