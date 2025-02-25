8b75dce938b95bdf3c2f3becf71abd1c2c657c93
Mostafa Sakr <msakr3@gmail.com>
2025-02-24 17:32:22 -0700

    Modify import.

--- app/frontend/components/RegistrationSession/AdminOnly/RegistrationSessionForm.jsx
+++ app/frontend/components/RegistrationSession/AdminOnly/RegistrationSessionForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
