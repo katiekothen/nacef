1e97a8b7cc46977d126f32c4326a6bcbf8f31822
Mostafa Sakr <msakr3@gmail.com>
2025-02-25 12:31:14 -0700

    Update Spanish localization further and create a js function to capitalize date string for enrollment index.

--- app/frontend/components/RegistrationSession/ListRegistrationSessions.jsx
+++ app/frontend/components/RegistrationSession/ListRegistrationSessions.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Card, Col } from "react-bootstrap";
 import UserListUI from '~/components/RegistrationSession/UserListUI.jsx';
+import CapitalizeDateDaySpanish from "~/components/RegistrationSession/Common/CapitalizeDateDaySpanish.jsx";
   
 const ListRegistrationSessions = (user, registrationSessions, setDeletePath, setDeleteMessage, setDisplayConfirmationModal, locale) => {
     return registrationSessions.map((registrationSession) => (
       <Col key={registrationSession.id}>
         <Card className="registration-session-card" bg="light" text="dark" border="dark">
           <Card.Body style={{ textAlign: "center" }}>
-            <Card.Subtitle>{registrationSession.date}<br />{registrationSession.start_time} — {registrationSession.end_time}</Card.Subtitle>
+            <Card.Subtitle>{CapitalizeDateDaySpanish(registrationSession.date, locale)}<br />{registrationSession.start_time} — {registrationSession.end_time}</Card.Subtitle>
             <br />
             {UserListUI(user, registrationSession, setDeletePath, setDeleteMessage, setDisplayConfirmationModal, locale)}
           </Card.Body>
